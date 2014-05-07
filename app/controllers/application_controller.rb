2b537f524dd7d63e6fb1e7577c73b8efcfbb332e
Fábio Moritz de Almeida <phabio_almeida@hotmail.com>
2014-05-07 18:00:16 +0000

    HLS support added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def get_ip_address
+    `ifconfig`.match(/inet addr:(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})/)[1]
+  end
+
+
 end
